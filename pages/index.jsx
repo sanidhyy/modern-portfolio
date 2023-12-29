ecc2e40f4d5c4b41e88e0c3cf99e474c9344781a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-29 08:38:31 +0000

    js -> jsx

---
+++ pages/index.jsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
+
+import { fadeIn } from "../variants";
+
+const Home = () => {
+  return (
+    <div className="bg-primary/60 h-full">
+      {/* text */}
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+          {/* title */}
+          <h1 className="h1">
+            Transforming Ideas <br /> Into{" "}
+            <span className="text-accent">Digital Reality</span>
+          </h1>
+
+          {/* subtitle */}
+          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate,
+            exercitationem harum, quia nulla temporibus deleniti libero veniam
+            vero beatae numquam ducimus illum ab similique ipsam tempore fugit
+            quod laudantium debitis.
+          </p>
+
+          {/* btn */}
+          <div className="flex justify-center xl:hidden relative">
+            <ProjectsBtn />
+          </div>
+          <motion.div
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex"
+          >
+            <ProjectsBtn />
+          </motion.div>
+        </div>
+      </div>
+      {/* image */}
+      <div className="">image</div>
+    </div>
+  );
+};
+
+export default Home;
