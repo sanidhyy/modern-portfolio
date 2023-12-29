65fc2eff1df8b7b7778c779e5bc9b34e97e42bc8
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-29 08:40:52 +0000

    rounded text implement

--- components/ProjectsBtn.jsx
+++ components/ProjectsBtn.jsx
@@ -1,5 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { HiArrowRight } from "react-icons/hi2";
+
 const ProjectsBtn = () => {
-  return <div>Project Btn</div>;
+  return (
+    <div className="mx-auto xl:mx-0">
+      <Link
+        href="/work"
+        className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group"
+      >
+        <Image
+          src="/rounded-text.png"
+          alt="rounded text"
+          width={141}
+          height={148}
+          className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"
+        />
+        <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300" />
+      </Link>
+    </div>
+  );
 };
 
 export default ProjectsBtn;
--- pages/index.jsx
+++ pages/index.jsx
@@ -14,18 +14,30 @@ const Home = () => {
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* title */}
-          <h1 className="h1">
+          <motion.h1
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
+          >
             Transforming Ideas <br /> Into{" "}
             <span className="text-accent">Digital Reality</span>
-          </h1>
+          </motion.h1>
 
           {/* subtitle */}
-          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
+          <motion.p
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate,
             exercitationem harum, quia nulla temporibus deleniti libero veniam
             vero beatae numquam ducimus illum ab similique ipsam tempore fugit
             quod laudantium debitis.
-          </p>
+          </motion.p>
 
           {/* btn */}
           <div className="flex justify-center xl:hidden relative">
