00f562df0469e58d35047de444f015c87dd29704
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-31 17:11:10 +0000

    work page done

--- components/WorkSlider.jsx
+++ components/WorkSlider.jsx
@@ -1,43 +1,51 @@
-// data
+import Image from "next/image";
+import { BsArrowRight } from "react-icons/bs";
+import { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
 const workSlides = {
   slides: [
     {
       images: [
         {
-          title: 'title',
-          path: '/thumb1.jpg',
+          title: "title",
+          path: "/thumb1.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb2.jpg',
+          title: "title",
+          path: "/thumb2.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb3.jpg',
+          title: "title",
+          path: "/thumb3.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb4.jpg',
+          title: "title",
+          path: "/thumb4.jpg",
         },
       ],
     },
     {
       images: [
         {
-          title: 'title',
-          path: '/thumb4.jpg',
+          title: "title",
+          path: "/thumb4.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb1.jpg',
+          title: "title",
+          path: "/thumb1.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb2.jpg',
+          title: "title",
+          path: "/thumb2.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb3.jpg',
+          title: "title",
+          path: "/thumb3.jpg",
         },
       ],
     },
@@ -45,7 +53,61 @@ const workSlides = {
 };
 
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
+  return (
+    <Swiper
+      spaceBetween={10}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="h-[280px] sm:h-[480px]"
+    >
+      {workSlides.slides.map((slide, i) => (
+        <SwiperSlide key={i}>
+          <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
+            {slide.images.map((image, imageI) => (
+              <div
+                className="relative rounded-lg overflow-hidden flex items-center justify-center group"
+                key={imageI}
+              >
+                <div className="flex items-center justify-center relative overflow-hidden group">
+                  {/* image */}
+                  <Image
+                    src={image.path}
+                    alt={image.title}
+                    width={500}
+                    height={300}
+                  />
+
+                  {/* overlay gradient */}
+                  <div
+                    className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"
+                    aria-hidden
+                  />
+
+                  {/* title */}
+                  <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
+                    <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
+                      {/* title part 1 */}
+                      <div className="delay-100">LIVE</div>
+                      {/* title part 2 */}
+                      <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
+                        PROJECT
+                      </div>
+                      {/* icon */}
+                      <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
+                        <BsArrowRight />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 };
 
 export default WorkSlider;
