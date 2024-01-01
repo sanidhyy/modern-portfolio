36d7d17cdf472fc96fc5c47703a53a7f73231e4d
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-01 09:10:13 +0000

    broken link fix in work page

--- components/WorkSlider.jsx
+++ components/WorkSlider.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { BsArrowRight } from "react-icons/bs";
 import { Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -14,18 +15,22 @@ const workSlides = {
         {
           title: "title",
           path: "/thumb1.jpg",
+          link: "http://example.com",
         },
         {
           title: "title",
           path: "/thumb2.jpg",
+          link: "http://example.com",
         },
         {
           title: "title",
           path: "/thumb3.jpg",
+          link: "http://example.com",
         },
         {
           title: "title",
           path: "/thumb4.jpg",
+          link: "http://example.com",
         },
       ],
     },
@@ -34,18 +39,22 @@ const workSlides = {
         {
           title: "title",
           path: "/thumb4.jpg",
+          link: "http://example.com",
         },
         {
           title: "title",
           path: "/thumb1.jpg",
+          link: "http://example.com",
         },
         {
           title: "title",
           path: "/thumb2.jpg",
+          link: "http://example.com",
         },
         {
           title: "title",
           path: "/thumb3.jpg",
+          link: "http://example.com",
         },
       ],
     },
@@ -64,7 +73,7 @@ const WorkSlider = () => {
     >
       {workSlides.slides.map((slide, i) => (
         <SwiperSlide key={i}>
-          <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
+          <div className="grid grid-cols-2 grid-rows-2 gap-4">
             {slide.images.map((image, imageI) => (
               <div
                 className="relative rounded-lg overflow-hidden flex items-center justify-center group"
@@ -87,7 +96,12 @@ const WorkSlider = () => {
 
                   {/* title */}
                   <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
-                    <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
+                    <Link
+                      href={image.link}
+                      target="_blank"
+                      rel="noreferrer noopener"
+                      className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]"
+                    >
                       {/* title part 1 */}
                       <div className="delay-100">LIVE</div>
                       {/* title part 2 */}
@@ -98,7 +112,7 @@ const WorkSlider = () => {
                       <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
                         <BsArrowRight aria-hidden />
                       </div>
-                    </div>
+                    </Link>
                   </div>
                 </div>
               </div>
